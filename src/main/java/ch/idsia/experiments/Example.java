c9fd999e72dea1d0f9c43861f08982e26f51fb89
Rafael Cabañas <rcabanas@idsia.ch>
2021-01-12 12:51:01 +0100

    first commit

---
+++ src/main/java/ch/idsia/experiments/Example.java
@@ -0,0 +1,8 @@
+package ch.idsia.experiments;
+import ch.idsia.crema.factor.credal.vertex.VertexFactor;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("example class");
+    }
+}
