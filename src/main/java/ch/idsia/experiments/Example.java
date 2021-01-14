f86a5f1bb36927f0ee49ca997e27b26ea3d006fb
Rafael Cabañas <rcabanas@idsia.ch>
2021-01-14 12:48:51 +0100

    structure generator

--- src/main/java/ch/idsia/experiments/Example.java
+++ src/main/java/ch/idsia/experiments/Example.java
@@ -1,8 +1,25 @@
 package ch.idsia.experiments;
-import ch.idsia.crema.factor.credal.vertex.VertexFactor;
+import ch.idsia.crema.factor.bayesian.BayesianFactor;
+import ch.idsia.crema.model.graphical.SparseModel;
+import ch.idsia.crema.model.io.bif.XMLBIFParser;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Example {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
         System.out.println("example class");
+
+        XMLBIFParser parser = new XMLBIFParser();
+		FileInputStream fio = new FileInputStream("networks/Benchmark-long1.xml");
+
+        SparseModel<BayesianFactor> model = parser.parse(fio);
+
+        System.out.println(model);
+
+
     }
 }
---
+++ src/main/java/ch/idsia/experiments/benchmark/GenStructure.java
@@ -0,0 +1,64 @@
+package ch.idsia.experiments.benchmark;
+
+import ch.idsia.crema.model.generator.BNGenerator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GenStructure {
+
+    // constant
+    static int nGraphs = 3;
+    static int maxInducedWidth = 8;
+
+
+    // todo check random seed: dependand of the parameters
+
+    public static void main(String[] args) throws Exception {
+        String[] structs = {"singly", "multi"}; // singly (polytree), multi
+        int[] maxInDegree = {2,4,6};
+        int[] numNodes = {4,6,8,10};
+        int[] maxDegree = {6};
+        int[] maxValues = {3,4,5};
+
+
+        for(int n: numNodes){
+            for(int mID: maxInDegree){
+                for(int mD: maxDegree){
+                    for(int mV: maxValues) {
+                        for(String structure : structs){
+                            generate(structure, n, mID, mD, mV);
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
+    }
+
+    public static void generate(String structure, int n, int mID, int mD, int mV) throws Exception {
+        String param_str = structure.substring(0,4)+"_n"+n+"_mID"+mID+"_mD"+mD+"_mV"+mV+"-";
+        String format = "xml"; // default format
+        String baseFileName = "networks/precise/bnet-"+param_str;
+
+        System.out.println("\n"+baseFileName);
+
+
+        BNGenerator gen = new BNGenerator(n, mD);
+
+        //// Generation process /////
+        // Optional parameters
+        Map kwargs = new HashMap<String, String>();
+        kwargs.put("maxInducedWidth", String.valueOf(maxInducedWidth));
+        kwargs.put("nGraphs", String.valueOf(nGraphs));
+
+
+        // start
+        gen.generate(structure, mV, baseFileName, format, kwargs);
+
+        System.out.println("Networks generated!");
+    }
+}
